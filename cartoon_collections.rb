9e059e65ccc1eebe35db5a3bb2ec9d278b868f91
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-27 17:07:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,16 @@ def summon_captain_planet(array)
   nu_array
 end
 
+def long_planeteer_calls(array) 
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
+end
+
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
     array.find do |type|
