4983cd2bb6faa0021f5b1bfe815124c18e5f3273
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-27 16:58:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,8 @@ def summon_captain_planet(array)
   nu_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(word)
+  word.any?(4)
 end
 
 def find_the_cheese# code an argument here
